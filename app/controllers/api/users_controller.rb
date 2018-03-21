e4c674261c3dec8ddbf1d09a3a51e63dd6c22189
Andrew Locke <alocke12992@gmail.com>
2018-03-21 13:37:05 -0600

    connect with aws

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,13 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
+
   def update
-    binding.pry
+    user = User.find(params[:id])
+    user.name = params[:name]
+    user.email = params[:email]
+    user.gamertag = params[:gamertag]
+    s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION']) 
+    s3_bucket = ENV['BUCKET']
+    file = params[:file]
   end
 end
